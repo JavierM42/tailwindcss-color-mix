282c80f7cd1ee6fc50d7ec1ed1d8defb9c6f8a82
fluqont <fluqont@gmail.com>
2025-02-06 22:20:37 +0300

    test: more opacity and arbitrary colors tests

--- tests/test.js
+++ tests/test.js
@@ -176,8 +176,38 @@ describe("tailwindcss-color-mix", () => {
     });
   });
 
-  describe("With arbitrary values in colors", () => {
-    it("defined specified class names", async () => {
+  describe("with opacity and arbitrary colors", () => {
+    it("doesn't break arbitrary colors", async () => {
+      const config = {
+        content: [
+          {
+            raw: "bg-[#ff0000]",
+          },
+        ],
+        theme,
+        plugins: [
+          colorMix({
+            bgMix: "bg-overlay",
+            bgMixAmount: "overlay-amount",
+            bgMixMethod: "overlay-method",
+          }),
+        ],
+      };
+
+      let utilitiesCSS = await postcss([require("tailwindcss")(config)])
+        .process("@tailwind utilities", { from: undefined })
+        .then((result) => result.css);
+
+      expect(utilitiesCSS.replace(/\n|\s|\t/g, "")).toBe(
+        `
+          .bg-\\[\\#ff0000\\]  {
+            background-color: var(--tw-bg-base);
+            --tw-bg-opacity: 1;
+            --tw-bg-base: rgb(255 0 0 / var(--tw-bg-opacity, 1))
+          }`.replace(/\n|\s|\t/g, "")
+      );
+    });
+    it("mixing works with arbitrary colors", async () => {
       const config = {
         content: [
           {
@@ -204,6 +234,77 @@ describe("tailwindcss-color-mix", () => {
             background-color: var(--tw-bg-base);
             --tw-bg-opacity: 1;
             --tw-bg-base: rgb(255 0 0 / var(--tw-bg-opacity, 1))
+          }`.replace(/\n|\s|\t/g, "")
+      );
+    });
+    it("works with opacity", async () => {
+      const config = {
+        content: [
+          {
+            raw: "bg-black bg-mix-white bg-mix-amount-50",
+          },
+        ],
+        theme,
+        plugins: [
+          colorMix({
+            bgMix: "bg-overlay",
+            bgMixAmount: "overlay-amount",
+            bgMixMethod: "overlay-method",
+          }),
+        ],
+      };
+
+      let utilitiesCSS = await postcss([require("tailwindcss")(config)])
+        .process("@tailwind utilities", { from: undefined })
+        .then((result) => result.css);
+
+      expect(utilitiesCSS.replace(/\n|\s|\t/g, "")).toBe(
+        `
+          .bg-black  {
+            background-color: rgb(0 0 0 / var(--tw-bg-opacity, 1));
+            background-color: var(--tw-bg-base);
+            --tw-bg-opacity: 1;
+            --tw-bg-base: rgb(0 0 0 / var(--tw-bg-opacity, 1))
+          }`.replace(/\n|\s|\t/g, "")
+      );
+    });
+    it("works with opacity and arbitrary colors", async () => {
+      const config = {
+        content: [
+          {
+            raw: "bg-black bg-opacity-50 bg-mix-white bg-mix-amount-50",
+          },
+        ],
+        theme,
+        plugins: [
+          colorMix({
+            bgMix: "bg-overlay",
+            bgMixAmount: "overlay-amount",
+            bgMixMethod: "overlay-method",
+          }),
+        ],
+      };
+
+      let utilitiesCSS = await postcss([require("tailwindcss")(config)])
+        .process("@tailwind utilities", { from: undefined })
+        .then((result) => result.css);
+
+      expect(utilitiesCSS.replace(/\n|\s|\t/g, "")).toBe(
+        `
+          .bg-black  {
+            --tw-bg-opacity: 1;
+            background-color: rgb(0 0 0 / var(--tw-bg-opacity, 1))
+          }
+          .bg-opacity-50 {
+            --tw-bg-opacity: 0.5
+          }
+          .bg-black {
+            background-color: var(--tw-bg-base);
+            --tw-bg-opacity: 1;
+            --tw-bg-base: rgb(0 0 0 / var(--tw-bg-opacity, 1))
+          }
+          .bg-opacity-50 {
+            --tw-bg-opacity: 0.5
           }`.replace(/\n|\s|\t/g, "")
       );
     });
